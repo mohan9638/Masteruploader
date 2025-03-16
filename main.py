05062e744846b649864fea642305b78bc7cd6ed6
nikhilsainiop <nikhilbhaiop02@gmail.com>
2025-03-16 17:21:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,11 @@ if __name__ == "__main__":
         await bot.start()
         bot_info = await bot.get_me()
         LOGGER.info(f"<--- @{bot_info.username} Started --->")
-        for user_id in Config.AUTH_USERS:
-            try:
-                await bot.send_message(chat_id=user_id, text=f"__Congrats! You Are DRM member ... if You get any error then contact me -  {Config.CREDIT}__ ")
-            except Exception as e:
-                LOGGER.error(f"Failed to send message to user {user_id}: {e}")
-                continue
+        try:
+            await bot.send_message(chat_id=user_id, text=f"__Congrats! You Are DRM member ... if You get any error then contact me -  {Config.CREDIT}__ ")
+        except Exception as e:
+            LOGGER.error(f"Failed to send message to user {user_id}: {e}")
+            continue
         await idle()
     asyncio.get_event_loop().run_until_complete(main())
     LOGGER.info("<--- Bot Stopped --->")
